7e4786e2d3d88d8ad2ee4dc6f68d0e45e7f0fa46
alexmeys <47882875+alexmeys@users.noreply.github.com>
2023-12-05 11:51:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,56 +3,51 @@ function toggleMenu() {
   nav.classList.toggle('active');
 }
 
-var menuToggle = document.querySelector('.menu-toggle');
-var overlayMenu = document.querySelector('.overlay-menu');
-
-menuToggle.addEventListener('click', function() {
-  overlayMenu.style.display = 'block';
-});
-
-overlayMenu.addEventListener('click', function() {
-  overlayMenu.style.display = 'none';
-});
-
-var links = document.querySelectorAll('a[href^="#"]');
-
-links.forEach(function(link) {
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-    var target = document.querySelector(this.getAttribute('href'));
-    var offset = target.offsetTop;
-    var delay = 1000; /* Set the delay time in milliseconds */
-    window.scrollTo({
-      top: offset,
-      behavior: 'smooth'
-    });
-    setTimeout(function() {
-      target.focus();
-    }, delay);
-  });
-});
-
+// Variable -> instellen
 var menuToggle = document.querySelector('.menu-toggle');
 var closeBtn = document.querySelector('.close-btn');
 var overlayMenu = document.querySelector('.overlay-menu');
 
-menuToggle.addEventListener('click', function() {
+// Overlay Menu weergeven
+function showOverlayMenu() {
   overlayMenu.style.display = 'block';
   menuToggle.classList.add('active');
   closeBtn.classList.add('active');
-});
+}
 
-closeBtn.addEventListener('click', function() {
+// Overlay Menu verbergen
+function hideOverlayMenu() {
   overlayMenu.style.display = 'none';
   menuToggle.classList.remove('active');
   closeBtn.classList.remove('active');
+}
+
+// Event acties
+menuToggle.addEventListener('click', showOverlayMenu);
+closeBtn.addEventListener('click', hideOverlayMenu);
+
+// Sluiten hamburger menu buiten x
+overlayMenu.addEventListener('click', function (event) {
+  if (event.target === overlayMenu) {
+    hideOverlayMenu();
+  }
 });
 
-// Add event listener to close the overlay when clicking outside the menu
-overlayMenu.addEventListener('click', function(event) {
-  if (event.target === overlayMenu) {
-    overlayMenu.style.display = 'none';
-    menuToggle.classList.remove('active');
-    closeBtn.classList.remove('active');
-  }
+// Smooth scrolling for anchor links
+var links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(function (link) {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    var target = document.querySelector(this.getAttribute('href'));
+    var offset = target.offsetTop;
+    var delay = 1000; /*1 second delay op scrollen (smooth)*/
+    window.scrollTo({
+      top: offset,
+      behavior: 'smooth'
+    });
+    setTimeout(function () {
+      target.focus();
+    }, delay);
+  });
 });
